89f7745382ce5a3f20bd2afeaad081fda42ea70e
Hypercubed <hypercubed@gmail.com>
2013-06-21 19:21:44 +0900

    Testing

--- wc-lazy.js
+++ wc-lazy.js
@@ -1,28 +1,94 @@
-// Input: A stream and a done call back
+﻿// Input: A stream and a done call back
 // Output: Nothing
 (function() {
 	var debug = false;
 
-	//Lazy = require('lazy');
+	Lazy = require('lazy');
 
-	function wcStream(reader, cb) {
-		var l = 0, w= 0, c = 0;
+	function wcStream(reader, opts, done) {
+
+	
+		done = (arguments.length < 3) ? opts : done;
+		opts = (arguments.length == 3) ? opts : { };
+		
+		if (!opts.words && !opts.lines && !opts.bytes && !opts.maxLineLength) {
+			opts.words = opts.lines = opts.bytes = true;
+			opts.maxLineLength = false;
+		} else {
+			opts.words = opts.words || false;
+			opts.lines = opts.lines || false;
+			opts.bytes = opts.bytes || false;
+			opts.maxLineLength = opts.maxLineLength || false;
+		}
+
+		var l = 0, w= 0, c = 0, L = 0;
+		var wb = true;
+		
+		var _count = function(data) {
+				debug &&
+					console.log('reader data');
+				
+				if (!data) return;
+				
+				var i = data.length;
+				
+				if (opts.bytes) c += i;
+				if (opts.maxLineLength) L = Math.max(i, L);
+				if (opts.lines) l += (data.match(/[\n]+/g) || '').length;
+				if (opts.words) w += (data.match(/[\s\n]+/g) || '').length;
+				
+				/* while (i--) {
+					var ch = data[i];
+					if (ch === '\n') l++;
+					
+					if (/[\s\n]/.test(ch)) {
+						wb = true;
+					} else {
+						if (wb) w++;
+						wb = false;
+					}
+				} */
+				
+			}
+			
+		var _end = function(_) {
+				debug && console.log('reader end');
+				
+				var r = [];
+				
+				if (opts.lines) r.push(l);
+				if (opts.words) r.push(w);
+				if (opts.bytes) r.push(c);
+				if (opts.maxLineLength) r.push(L);
+				
+				if (done) 
+					done(null, r);
+			}
 		
 		debug && console.log('reader start');
+		
 		reader
-			.on('data', function(data) {
-				debug && console.log('npmreader data', data.length);
-				if (!data) return;
-				c += data.length;
-				l += (data.match(/\n+/g) || '').length;
-				w += (data.match(/[\s\n]+/g) || '').length;
-			})
-			.on('end', function(_) {
-				debug && console.log('reader end');
-				if (cb) 
-					cb(null, [l, w, c]);
-			})
-			.resume();
+			//.on('data', _count)
+			.on('end', _end)
+			;
+			
+		new Lazy(reader)
+			.filter(function(data) { return !!data; })
+			.forEach(function(data) { if (opts.bytes) c += data.length; })
+			//.map(function(chunk) {  // Bug in lazy: http://stackoverflow.com/questions/16755192/node-js-lazy-how-to-prevent-an-empty-line-being-read-as-0
+				// replace Unix-style (LF) line ending with DOS-style (CRLF)
+			//	return chunk.replace(/(?!\r)\n/g, '\r\n');
+			//})
+			.lines
+				.forEach(function(line) { if (opts.lines) l++; })
+				.filter(function(line) { return !(line == "0"); })  // bug
+				.map(String)
+				//.forEach(function(line) { console.log(line) })
+				.forEach(function(line) { if (opts.words) w += (line.match(/[^\s\n]+/g) || '').length; })
+				.forEach(function(line) { if (opts.maxLineLength) L = Math.max(L,line.length); })
+				;
+			
+		reader.resume();
 		
 	}
 
--- wc.js
+++ wc.js
@@ -10,10 +10,14 @@ var program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-c, --bytes', 'print the byte counts')
-  .option('-w, --words', 'print the newline counts')
-  .option('-l, --lines', 'print the word counts')
+  .option('-c, --bytes', 'print the byte counts', false)			// Todo: need to respect these
+  .option('-w, --words', 'print the newline counts', false)
+  .option('-l, --lines', 'print the word counts', false)
+  .option('-L, --max-line-length', 'print the length of the longest line', false)		// Todo
   .parse(process.argv);
+  
+//console.log(program);
+//process.exit();
 
 var path = require('path'), 
 	fs = require('fs');
@@ -21,36 +25,48 @@ var path = require('path'),
 var files = program.args;
 if (files.length == 0) files = [ '-' ];
 
-var total = [0,0,0];
+var total = [];
+var i = files.length;
 
-files.forEach(function(filename) {
+var getStream = function(filename) {
 	if (filename == "-") {
 		debug && console.log('stdin');
 		
-		reader = process.stdin;
+		var reader = process.stdin;
 		process.stdin.setEncoding('utf8');
 
-		/* process.stdin.on('data', function (chunk) {
-			process.stdout.write('data: ' + chunk);
-		});
-		process.stdin.on('end', function () {
-			process.stdout.write('end');
-		});
-		return; */
-		
 	} else {
 		var filepath = path.join(__dirname, filename);
 		var reader = fs.createReadStream(filepath, {"encoding": 'utf-8', "flags": 'r', "fd": null});
 	}
+	
+	return reader;
+}
 
-	wc(reader, function(err, data) {
-		total = data.map(function(d,i) {
-			total[i] += d;
+var print = function(counts, filename) {
+	var output = counts.join(' ')+' '+filename;
+	console.log(output);
+}
+
+var countFile = function(filename) {
+	debug && 
+		console.log('countFile');
+		
+	
+	
+	var reader = getStream(filename);
+	wc(reader, program, function(err, data) {
+		data.forEach(function(d,i) {
+			total[i] = (total[i] || 0) + d;
 		});
-		console.log(data.join(' ')+' '+filename);
-	});
-});
+		
+		print(data, filename != '-' ? filename : '');
+		
+		if (!--i && files.length > 1)
+			print(total, 'total');
 
-//if (files.length > 1) {
-	//console.log(total.join(' ')+' total');
-//
+	});
+
+}
+
+files.forEach(countFile);
